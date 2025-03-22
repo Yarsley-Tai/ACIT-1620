8045a6b9352cc23c8d2e39803de0384f853358ff
Justin Dai <Ytai10@my.bcit.ca>
2025-03-21 21:24:08 -0700

    Test

---
+++ Week11/index.js
@@ -0,0 +1,11 @@
+let dogName = "Spot";
+dogWeight = 17;
+bark(spot, 17);
+
+function bark(name, weight) {
+  if (weight > 20) {
+    console.log(name + " says WOOF WOOF");
+  } else {
+    console.log(name + " says woof woof");
+  }
+}
